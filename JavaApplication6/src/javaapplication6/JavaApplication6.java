194faf906389df5bb68e972fc059ee744ba67dd2
ogalvezgonzalez <ogalvezgonzalez@victoria28.danielcastelao.org>
2017-12-15 09:58:30 +0100

    Commit F

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -17,6 +17,7 @@ public class JavaApplication6 {
     public static void main(String[] args) {
         System.out.println("Examen");
         System.out.println("Intermedio");
+        System.out.println("Y terminamos");
     }
     
 }
